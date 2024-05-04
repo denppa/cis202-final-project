70ac78881da6b743f286bd1746311894d909ba2b
Jimmy Choi <jimmy.choi@ganjing.dev>
2024-05-03 21:56:28 -0400

    Main logic added.

--- main.go
+++ main.go
@@ -9,11 +9,15 @@ import (
 
 func main() {
 	fmt.Println(`go run main.go mode arg
-	mode: ls
+	mode: ls "path/to/list"
+	excel: excel "path/to/list" "save_file_name"
 	`)
 	mode := os.Args[1]
 	if mode == "ls" {
 		fmt.Println(handle.Ls(os.Args[2]))
+	} else if mode == "excel" {
+		handle.Excel(handle.Ls(os.Args[2]), os.Args[3])
 	}
+
 }
 
