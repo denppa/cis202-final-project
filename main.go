f5d8cede03a3c26985c266d33d77eb6ee71276c3
Jimmy Choi <jimmy.choi@ganjing.dev>
2024-05-03 22:16:13 -0400

    Main now accepts excelMvDel mode.

--- main.go
+++ main.go
@@ -11,12 +11,15 @@ func main() {
 	fmt.Println(`go run main.go mode arg
 	mode: ls "path/to/list"
 	excel: excel "path/to/list" "save_file_name"
+	excelMvDel: excelMvDel "with_actions_column.xlsx"
 	`)
 	mode := os.Args[1]
 	if mode == "ls" {
 		fmt.Println(handle.Ls(os.Args[2]))
 	} else if mode == "excel" {
 		handle.Excel(handle.Ls(os.Args[2]), os.Args[3])
+	} else if mode == "excelMvDel" {
+		handle.ExcelMvDel(os.Args[2])
 	}
 
 }
